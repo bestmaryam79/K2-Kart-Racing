c6a25d2bfb654f2f680ee34e02ec0896657aebac
bestmaryam79 <101430uk@saeinstitute.edu>
2023-03-22 22:09:48 +0000

    adding more scripts

---
+++ Kart-Racing-Unity-Game-967803688b20d206ea3ba247ea5c508778cdd57d/Assets/AI Controller.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+
+public class AIController : MonoBehaviour
+{
+    // Variables to store references to the player's kart and the track.
+    public GameObject playerKart;
+    public GameObject track;
+
+    // Speed and steering variables.
+    public float maxSpeed = 10f;
+    public float turnSpeed = 100f;
+
+    // Rule-based system variables.
+    public float distanceThreshold = 5f;
+    public float turnThreshold = 10f;
+    public float avoidMultiplier = 5f;
+
+    // Update is called once per frame.
+    void Update()
+    {
+        // Get the AI kart's position.
+        Vector3 currentPosition = transform.position;
+
+        // Calculate the distance to the player's kart.
+        float playerDistance = playerDirection.magnitude;
+
+        // Calculate the angle to the player's kart.
+        float playerAngle = Vector3.Angle(transform.forward, playerDirection);
+
+        // Get the position of the next waypoint on the track.
+        Vector3 nextWaypoint = track.GetComponent<TrackManager>().GetNextWaypoint(currentPosition);
+
+        // Calculate the direction to the next waypoint.
+        Vector3 waypointDirection = nextWaypoint - currentPosition;
+        waypointDirection.y = 0f;
+
+        // Calculate the distance to the next waypoint.
+        float waypointDistance = waypointDirection.magnitude;
+
+        // Calculate the angle to the next waypoint.
+        float waypointAngle = Vector3.Angle(transform.forward, waypointDirection);
+
+        // Calculate the steering input based on the rules.
+        float steeringInput = 0f;
+
+        if (playerDistance < distanceThreshold)
+        {
+            if (playerAngle < turnThreshold)
+            {
+                steeringInput = -1f;
+            }
+            else
+            {
+                steeringInput = 1f;
+            }
+
+            steeringInput *= avoidMultiplier;
+        }
+        else
+        {
+            if (waypointAngle < turnThreshold)
+            {
+                steeringInput = -1f;
+            }
+            else
+            {
+                steeringInput = 1f;
+            }
+        }
+
+        // Apply the steering input to the AI kart's rotation.
+        transform.Rotate(0f, steeringInput * turnSpeed * Time.deltaTime, 0f);
+
+        // Apply the maximum speed to the AI kart's movement.
+        transform.Translate(0f, 0f, maxSpeed * Time.deltaTime);
+    }
+}
